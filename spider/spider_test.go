f5de43b0a70da9d7e44c5b874c309068e6f0c475
郑守迪 <davis.zheng@ucloud.cn>
2023-12-01 22:50:35 +0800

    tmp

--- spider/spider_test.go
+++ spider/spider_test.go
@@ -2,15 +2,15 @@ package spider
 
 import (
 	"fmt"
-	// "io/ioutil"
+	"io/ioutil"
 	"testing"
 )
 
 func TestGetHTML(t *testing.T) {
-	url := "http://www.31xiaoshuo.com/0/196/"
-	// spider := &Spider{url}
-	// html := spider.get_html_all()
-	// ioutil.WriteFile("test.txt", []byte(html), 0644)
+	url := "https://www.31xiaoshuo.com/0/196/"
+	spider := &Spider{url}
+	html := spider.get_html_all()
+	ioutil.WriteFile("test.txt", []byte(html), 0644)
 	chapter := GetLatestChapter(url)
 	fmt.Printf("\nchapter:%+v\n\n", chapter)
 	// GetNovel(url)
